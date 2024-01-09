2e87887a6114affcd02a0a4c8b38ac8c80838c1f
Zty <ztory@foxmail.com>
2024-01-09 12:43:47 +0800

    Add new group and user only if not exist

--- root/etc/cont-init.d/00-smb.sh
+++ root/etc/cont-init.d/00-smb.sh
@@ -6,9 +6,17 @@ set -o pipefail
 
 GROUP_NAME='time-machine-users'
 
-groupadd --non-unique --gid "$PGID" "$GROUP_NAME"
-useradd --system --non-unique --gid "$GROUP_NAME" --uid "$PUID" "$SMB_USER"
+egrep "^$GROUP_NAME" /etc/group >& /dev/null
+if [ $? -ne 0 ]
+then
+  groupadd --non-unique --gid "$PGID" "$GROUP_NAME"
+fi
 
+id $SMB_USER >& /dev/null
+if [ $? -ne 0 ]
+then
+  useradd --system --non-unique --gid "$GROUP_NAME" --uid "$PUID" "$SMB_USER"
+fi
 
 printf '%s\n%s\n' "$SMB_PASSWORD" "$SMB_PASSWORD" | smbpasswd -s -a "$SMB_USER"
 
